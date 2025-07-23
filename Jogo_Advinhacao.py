a0749d5fb36474c74ac3ae2aceeb03e9db932c26
Erick <erickmachado050@gmail.com>
2025-07-23 20:20:36 -0300

    Python escolhendo a palavra aleatóra

--- Jogo_Advinhacao.py
+++ Jogo_Advinhacao.py
@@ -1,2 +1,5 @@
+import random
 lista = ["mundo", "praia", "festa", "docei", "limão", "fruta", "manga", "fogão", "noite", "sonho", "campo", "grama", "vidro", "aluno", "tampa", "caixa", "livro", "chave", "tempo", "pedra", "cobra", "risco", "copos", "barca", "anéis"
 ]
+
+palavra_aleatoria = random.choice(lista)
