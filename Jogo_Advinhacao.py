e6d484fe3b7ee3503eb9767b346a1cdafba19375
Erick <erickmachado050@gmail.com>
2025-07-24 14:28:08 -0300

    Verificar se a palavra contém 5 letras

--- Jogo_Advinhacao.py
+++ Jogo_Advinhacao.py
@@ -1,5 +1,14 @@
 import random
-lista = ["mundo", "praia", "festa", "docei", "limão", "fruta", "manga", "fogão", "noite", "sonho", "campo", "grama", "vidro", "aluno", "tampa", "caixa", "livro", "chave", "tempo", "pedra", "cobra", "risco", "copos", "barca", "anéis"
+lista = ["mundo", "praia", "festa", "docei", "limao", "fruta", "manga", "fogao", "noite", "sonho", "campo", "grama", "vidro", "aluno", "tampa", "caixa", "livro", "chave", "tempo", "pedra", "cobra", "risco", "copos", "barca", "aneis"
 ]
 
 palavra_aleatoria = random.choice(lista)
+tentativas = 6
+while tentativas > 0:
+    print(f"Você tem {tentativas} tentativas")
+    advinha = input('Tente advinhar a palavra que estou pensando: ').lower().strip()
+    
+    if len(advinha) != len(palavra_aleatoria):
+        print(f'por favor digite uma palavra com {len(palavra_aleatoria)} letras')
+        continue
+    
