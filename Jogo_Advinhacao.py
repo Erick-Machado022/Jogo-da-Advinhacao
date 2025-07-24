7664eb86bfc8f4ba1517bce2765049a899f1c012
Erick <erickmachado050@gmail.com>
2025-07-24 17:48:09 -0300

    Add loopin para mais jogadas

--- Jogo_Advinhacao.py
+++ Jogo_Advinhacao.py
@@ -1,5 +1,12 @@
 import random, time
-lista = ["mundo", "praia", "festa", "docei", "limao", "fruta", "manga", "fogao", "noite", "sonho", "campo", "grama", "vidro", "aluno", "tampa", "caixa", "livro", "chave", "tempo", "pedra", "cobra", "risco", "copos", "barca", "aneis"
+lista = [
+    "limão", "carro", "roupa", "canal", "mudar", "nuvem", "pazez", "jogar",
+    "neves", "livro", "folha", "sorte", "andar", "fruta", "cinto", "pular",
+    "velho", "banco", "janes", "rádio", "festa", "matar", "troca", "vazio",
+    "corpo", "nadar", "risco", "perda", "sinal", "campo", "linha", "trigo",
+    "cofre", "casar", "chuva", "vinho", "pedra", "verde", "morro", "norte",
+    "sulco", "dente", "vapor", "fraco", "ruído", "feira", "magia", "verão",
+    "coisa", "tempo"
 ]
 
 print("Bem-vindo ao jogo de adivinhação de palavras!")
@@ -19,36 +26,41 @@ time.sleep(1.5)
 print("-="*20)
 
 
-palavra_aleatoria = random.choice(lista)
-tentativas = 6
-while tentativas > 0:
-    if tentativas > 1:
-        print(f"Você tem {tentativas} tentativas")
-    else:
-        print(f"Você tem {tentativas} tentativa")
-    advinha = input('Tente advinhar a palavra que estou pensando: ').lower().strip()
-    
-    if len(advinha) != len(palavra_aleatoria):
-        print(f'por favor digite uma palavra com {len(palavra_aleatoria)} letras')
-        continue
-    if advinha == palavra_aleatoria:
-            print(f'\033[32m{palavra_aleatoria}\033[m')
-            print('\033[32mParabéns\033[m! Você acertou a palavra')
-            break
-    for indice, letra in enumerate(advinha):
-        
-        if letra == palavra_aleatoria[indice]:
-            print(f'\033[32m{letra}\033[m', end=' ') #Se a letra estiver certa aparece verde
-            time.sleep(.2)
-        elif letra in palavra_aleatoria:
-            print(f'\033[33m{letra}\033[m', end=' ') # Se tiver a letra mas no lugar errado aparece amarelo
-            time.sleep(.2)
+while True:   
+    palavra_aleatoria = random.choice(lista)
+    print(palavra_aleatoria)
+    tentativas = 6
+    while tentativas > 0:
+        if tentativas > 1:
+            print(f"Você tem {tentativas} tentativas")
         else:
-            print(f'\33[31m{letra}\033[m', end=' ') # Se não t iver a letra aparece vermelho
-            time.sleep(.2)
-    print()
-    print('-='*10)
-    tentativas -= 1
-if tentativas == 0:
-    print(f'\n\033[31mVocê perdeu!\033[m A palavra era: {palavra_aleatoria}')
-
+            print(f"Você tem {tentativas} tentativa")
+        advinha = input('Tente advinhar a palavra que estou pensando: ').lower().strip()
+        
+        if len(advinha) != len(palavra_aleatoria):
+            print(f'por favor digite uma palavra com {len(palavra_aleatoria)} letras')
+            continue
+        if advinha == palavra_aleatoria:
+                print(f'\033[32m{palavra_aleatoria}\033[m')
+                print('\033[32mParabéns\033[m! Você acertou a palavra')
+                break
+        for indice, letra in enumerate(advinha):
+            
+            if letra == palavra_aleatoria[indice]:
+                print(f'\033[32m{letra}\033[m', end=' ') #Se a letra estiver certa aparece verde
+                time.sleep(.2)
+            elif letra in palavra_aleatoria:
+                print(f'\033[33m{letra}\033[m', end=' ') # Se tiver a letra mas no lugar errado aparece amarelo
+                time.sleep(.2)
+            else:
+                print(f'\33[31m{letra}\033[m', end=' ') # Se não t iver a letra aparece vermelho
+                time.sleep(.2)
+        print()
+        print('-='*10)
+        tentativas -= 1
+    if tentativas == 0:
+        print(f'\n\033[31mVocê perdeu!\033[m A palavra era: {palavra_aleatoria}')
+    resp = input('Deseja continuar?  [S/N]').lower()[0]
+    if resp == 'n':
+        break
+print('Obrigado por jogar!')
